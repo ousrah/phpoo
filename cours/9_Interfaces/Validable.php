636644be66a1644d407430b3d72e2a0b6f368494
oussama rahmouni <ousrah@hotmail.com>
2025-10-19 11:59:52 +0100

    cours

--- cours/9_Interfaces/Validable.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-/**
- * Interface Validable
- * Elle garantit que toute classe qui l'implémente aura la méthode validerCompte().
- */
-interface Validable {
-    public function validerCompte();
-}
